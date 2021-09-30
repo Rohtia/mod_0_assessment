f2651e294b174c00ffb07ac2b60d0704f1783e62
Rohtia <90939780+Rohtia@users.noreply.github.com>
2021-09-30 18:51:03 -0400

    Complete program specific challenge

---
+++ variables.js
@@ -0,0 +1,12 @@
+var famousPerson = "Walt Disney";
+var isActive = false;
+famousPerson = "Jim Henson";
+var num3 = 34;
+var num9 = 832;
+
+var sum = num9 + num3;
+var animals = ["bear", "cat", "dog"];
+
+animals.shift();
+
+console.log(famousPerson.length);
